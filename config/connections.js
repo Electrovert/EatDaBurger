fd6df0ef85ac51409d17e3955f44efae8da3a790
Amanda Foster <mandolinfoster@gmail.com>
2020-02-28 20:49:16 -0500

    Added connections.js

---
+++ config/connections.js
@@ -0,0 +1,30 @@
+// Requires mysql
+var mysql = require("mysql");
+
+var connection;
+
+// Creates connection
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        //Port
+        port: 3306,
+        // Username
+        user: "root",
+        // Password
+        password: "Kelf31107",
+        // DataBase
+        database: "burgers_db"
+    });
+};
+
+// Error handling
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+// Test comment
+module.exports = connection;
